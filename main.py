3b3f9eb68f45ce16e4aff7ce7f27eef2b5ae622d
Josip Feliks <josip.feliks89@gmail.com>
2023-10-31 22:53:54 +0100

    Read blockfrost project id from .env

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("this is my first message to the world. this will be a very useful app")
+from blockfrost import BlockFrostApi, ApiError, ApiUrls
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api = BlockFrostApi(
+    project_id=os.getenv("BLOCKFROST_PROJECT_ID"),
+    base_url=ApiUrls.mainnet.value,
+)
+
+print(os.environ["BLOCKFROST_PROJECT_ID"])
