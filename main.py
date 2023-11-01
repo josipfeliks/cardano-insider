3927614a32ef50878a02b8a53b85da90147d1633
Josip Feliks <josip.feliks89@gmail.com>
2023-11-02 00:04:43 +0100

    Detect which assets are nfts and which are native tokens

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from blockfrost import BlockFrostApi, ApiError, ApiUrls
 import os
 from dotenv import load_dotenv
+import codecs
 
 load_dotenv()
 
@@ -13,9 +14,22 @@ try:
     address = "addr1qxvwaujuw3vky80h0j73xwh466q63zx6t67efvvz4npn6py52y9ckt97pxqvvp0d7an8hdtc2w3nvpxswfvd44q9cusqklxjqt"
     addr = api.address(address)
 
+
+    decode_hex = codecs.getdecoder("hex_codec")
+
     for amount in addr.amount:
         if (amount.unit == "lovelace"):
             print(f'ADA balance on {address}: {int(amount.quantity) / 1000000} ADA')
+        else:
 
+            policy_id = amount.unit[0:56]
+            data = api.assets_policy(policy_id)
+            asset_name = decode_hex(amount.unit[56:])[0]
+
+            if (len(data) != 1):
+                print(f"NFT {asset_name}")
+            else:
+                print(f"Native token {asset_name}: {amount.quantity}")
+            
 except ApiError as e:
     print(e)
